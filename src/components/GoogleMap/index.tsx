f940020e3b213420290e3951f6f02a0e2a69b117
Asrul Kadir <asrulkadir.asus4@gmail.com>
2022-01-15 13:57:16 +0800

    add console log

--- src/components/GoogleMap/index.tsx
+++ src/components/GoogleMap/index.tsx
@@ -18,6 +18,8 @@ function MapComponent() {
     margin: 'auto',
   };
 
+  console.log(process.env.REACT_APP_GMAPS_API);
+
   return (
     <>
       <LoadScript googleMapsApiKey={`${process.env.REACT_APP_GMAPS_API}`}>
